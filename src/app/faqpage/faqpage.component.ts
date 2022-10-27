692392f4fc400af31bb8a6a935c9592802c83c51
Osvaldo G Del Rio <osdelrio_0@hotmail.com>
2022-10-27 15:25:14 -0700

    Localbusiness

--- src/app/faqpage/faqpage.component.ts
+++ src/app/faqpage/faqpage.component.ts
@@ -1,5 +1,5 @@
   import { Component, OnInit } from '@angular/core';
-  import { FormArray, FormBuilder, Validators } from '@angular/forms';
+  import { FormBuilder } from '@angular/forms';
   import { ClipboardService } from 'ngx-clipboard';
   
   @Component({
--- src/app/localbusiness/localbusiness.component.ts
+++ src/app/localbusiness/localbusiness.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { ClipboardService } from 'ngx-clipboard';
 
 @Component({
   selector: 'app-localbusiness',
@@ -7,9 +9,51 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LocalbusinessComponent implements OnInit {
 
-  constructor() { }
+  constructor(private fb: FormBuilder, private clipboardApi: ClipboardService) { }
 
-  ngOnInit(): void {
+    public business: any[] = [{
+      "@context": "http://schema.org",
+      "@type": "LocalBusiness",
+      name: '',
+      url: '',
+      description: '',
+      image: '',
+      telephone: '',
+      priceRange: '',
+      "address": {}
+    }];
+
+    public adress: any[] = [{  
+      "@type": "PostalAddress",
+      streetAddress: '',
+      addressLocality: '',
+      addressRegion: '',
+      postalCode: '',
+      addressCountry: "USA"
+    }];
+    
+    jsonSchema: any = {};
+    
+    form: any = [];
+
+    public qaItem: any[] = [];
+
+    ngOnInit(): void {
+      this.updateProfile();
+    }
+  
+    updateProfile() {
+      this.jsonSchema = '';
+      this.business[0].address = this.adress[0];
+      this.jsonSchema = this.business[0];
+    }
+  
+    copySCHEMA() {
+      this.clipboardApi.copyFromContent(JSON.stringify(this.jsonSchema));
+    }
+  
+    copySCHEMATAG() {
+      this.clipboardApi.copyFromContent('<script type="application/ld+json">' + JSON.stringify(this.jsonSchema) + '</script>' );
+    }
+  
   }
-
-}
